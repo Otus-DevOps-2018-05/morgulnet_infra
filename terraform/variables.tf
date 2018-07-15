f097c69b1b6fc7dfcf2f688995e05b368d733bd1
Sergey Volchkov <morgul@mail.ru>
2018-07-15 15:11:31 +0300

    Home work terraform1

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-west1"
+}
+
+variable public_key_path {
+  description = " SSH pub key path"
+}
+
+variable disk_image {
+  description = "Disk image"
+}
